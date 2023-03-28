849ded5fdc6b953920ab916aeefa510cfe186f87
milos-sedlacek <milo.sedlacek@gmail.com>
2023-03-28 16:50:24 +0200

    Added order

--- EmpowerMS_1/EmpowerMS_1.cs
+++ EmpowerMS_1/EmpowerMS_1.cs
@@ -55,6 +55,7 @@ namespace EmpowerMS_1
 	using System.Collections.Generic;
 	using System.Globalization;
 	using System.Text;
+	using Empower.Library.Room1;
 	using Skyline.DataMiner.Automation;
 	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
 
@@ -79,6 +80,9 @@ namespace EmpowerMS_1
 			{
 				engine.GenerateInformation(element.Name);
 			}
+
+			var order = OrderFactory.CreateOrder("Order 1");
+			order.Dispatch("Milos Sedlacek");
 		}
 	}
 }
